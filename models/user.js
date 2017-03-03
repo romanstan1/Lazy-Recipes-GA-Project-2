dcb641da3a43033f2bc35b921bb98a4af8220a9a
Roman Stankiewicz <roman.stan3@gmail.com>
2017-03-03 09:33:13 +0000

    intial commit

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: { type: String, required: true }
+});
+
+userSchema
+  .virtual('passwordConfirmation')// virtual because we dont want to store it in the database, use it remporatilay by setting it onto the record temporatily without storing it
+  .set(function setPasswordConfirmation(passwordConfirmation) {
+    this._passwordConfirmation = passwordConfirmation;
+  });
+
+
+//this is a lifecycle hook - mongoose middleware, this runs before mongoose trys to valdate the password in the userSchema ie (password: { type: String, required: true }), it fires this function before it validates because of .pre
+userSchema.pre('validate', function checkPassword(next) {
+  if(this._passwordConfirmation && this._passwordConfirmation !== this.password) this.invalidate('passwordConfirmation', 'does not match');
+  next();
+});
+
+userSchema.pre('validate', function checkPassword(next) {
+  if(!this.password && !this.githubId) {
+    this.invalidate('password', 'required');
+  }
+  if(this.isModified('password') && this._passwordConfirmation !== this.password) this.invalidate('passwordConfirmation', 'does not match');
+  next();
+});
+
+
+userSchema.pre('save', function hashPassword(next) {
+  if(this.isModified('password')) {
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+  }
+  next();
+});
+
+// A class is like the blue print, new Array() is the class, [] is the object instance
+//methods is creating an instance method on the User
+userSchema.methods.validatePassword = function validatePassword(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+const session = require('express-session');
+const flash = require('express-flash');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const { port, env, dbURI, sessionSecret } = require('./config/environment');
+const errorHandler = require('./lib/errorHandler');
+const routes = require('./config/routes');
+const customResponses = require('./lib/customResponses');
+const authentication = require('./lib/authentication');
+
+//create an express app
+const app = express();
+
+// set up our template engine
+app.set('view engine', 'ejs');
+app.set('views', `${__dirname}/views`);
+app.use(expressLayouts);
+
+//set up our static files folder. whenever you request anything first check whethet if its public. then mvoe on to routes
+app.use(express.static(`${__dirname}/public`));
+
+
+//connect to our database
+mongoose.connect(dbURI);
+
+//set up our middleware
+if(env !== 'test') app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(methodOverride(function (req) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    const method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
+
+
+//set up our sessions
+app.use(session({
+  secret: sessionSecret,
+  resave: false,
+  saveUninitialized: false
+}));
+
+// set up flash messages AFTER sessions
+app.use(flash());
+
+//set up custom middleware
+app.use(customResponses);
+app.use(authentication);
+
+
+//stick in the routes before error handler
+app.use(routes);
+
+// set up error handler ALWAYS LAST
+app.use(errorHandler);
+
+
+app.listen(port, () => console.log(`express is listening to port ${port}`));
