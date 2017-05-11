4cd31180c6933f4290641d99199e9f3430f706c4
Roman Stankiewicz <roman.stan3@gmail.com>
2017-05-11 08:45:11 +0100

    show page styling

--- src/js/app.js
+++ src/js/app.js
@@ -12,60 +12,47 @@ $(() => {
   });
 
 
-  // var scrollPos = 0;
-  // var animationBeginPos = 0;
-  // var animationEndPos = 1000;
-  //
-  // var beginningColor = new $.Color( 'rgb(210,50,98)' ); //we can set this here, but it'd probably be better to get it from the CSS; for the example we're setting it here.
-  // var endingColor = new $.Color( 'rgb(0,197,209)' ); //what color we want to use in the end
 
-  // $(document).scroll(function() {
-  //   scrollPos = $(this).scrollTop();
-  //   if(scrollPos >= animationBeginPos && scrollPos <= animationEndPos ) {
-  //        // console.log( 'scrolling and animating' );
-  //         //we want to calculate the relevant transitional rgb value
-  //     var percentScrolled = scrollPos / ( animationEndPos - animationBeginPos );
-  //     var newRed = beginningColor.red() + ( ( endingColor.red() - beginningColor.red() ) * percentScrolled );
-  //     var newGreen = beginningColor.green() + ( ( endingColor.green() - beginningColor.green() ) * percentScrolled );
-  //     var newBlue = beginningColor.blue() + ( ( endingColor.blue() - beginningColor.blue() ) * percentScrolled );
-  //     var newColor = new $.Color( newRed, newGreen, newBlue );
+  // $(window).scroll(function () {
+  //   let top = $(window).scrollTop();
+  //   console.log(top);
+  //   top = (top / 2) + 33;
   //
-  //     $('body').animate({ backgroundColor: newColor }, 0);
-  //   } else if ( scrollPos > animationEndPos ) {
-  //     $('body').animate({ backgroundColor: endingColor }, 0);
-  //   } else if ( scrollPos < animationBeginPos ) {
-  //     $('body').animate({ backgroundColor: beginningColor }, 0);
-  //   } else {
-  //     console.log('nothing');
-  //   }
+  //   rgba(33, 33, 51)
+  //
+  //
+  //   $('body').css('background', `rgba(33, 33, ${top}, 1)`);
+  //   // color: rgba(33, 33, 51, 1);
   // });
-  $('h5').addClass('madeBy');
 
-  if (window.location.pathname !== '/') {
-    $('body').addClass('changeColor');
-    $('h5').removeClass('madeBy');
-    $('header').removeClass('opacity');
-    $('h2.home').addClass('changeColorH2');
-    //$('h3.home').removeClass('changeColorH3');
-  }
 
-  $(window).scroll(function () {
-    if (($(this).scrollTop() > 50) && window.location.pathname==='/') {
-      $('body').addClass('changeColor');
-      $('h2.home').addClass('changeColorH2');
-      $('h3.home').addClass('changeColorH3');
-      $('h5').removeClass('madeBy');
-      $('header').removeClass('opacity');
-    }
-    if (($(this).scrollTop() < 50) && window.location.pathname==='/') {
-      $('body').removeClass('changeColor');
-      $('header').addClass('opacity');
-      $('h2.home').removeClass('changeColorH2');
-      $('h3.home').removeClass('changeColorH3');
-      //$('h5').addClass('madeBy');
-    }
-
-  });
+  // $('h5').addClass('madeBy');
+  //
+  // if (window.location.pathname !== '/') {
+  //   $('body').addClass('changeColor');
+  //   $('h5').removeClass('madeBy');
+  //   $('header').removeClass('opacity');
+  //   $('h2.home').addClass('changeColorH2');
+  //   //$('h3.home').removeClass('changeColorH3');
+  // }
+  //
+  // $(window).scroll(function () {
+  //   if (($(this).scrollTop() > 50) && window.location.pathname==='/') {
+  //     $('body').addClass('changeColor');
+  //     $('h2.home').addClass('changeColorH2');
+  //     $('h3.home').addClass('changeColorH3');
+  //     $('h5').removeClass('madeBy');
+  //     $('header').removeClass('opacity');
+  //   }
+  //   if (($(this).scrollTop() < 50) && window.location.pathname==='/') {
+  //     $('body').removeClass('changeColor');
+  //     $('header').addClass('opacity');
+  //     $('h2.home').removeClass('changeColorH2');
+  //     $('h3.home').removeClass('changeColorH3');
+  //     //$('h5').addClass('madeBy');
+  //   }
+  //
+  // });
 
 
 });
